0f74ffc74d130a726d575b66496f81834b81a0ca
Martijn <roo.martijn@gmail.com>
2014-10-05 12:50:39 +0300

    MongoDB database now used to store contacts

--- contacts_api.js
+++ contacts_api.js
@@ -1,74 +1,132 @@
 var express = require('express');
 var app = express();
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/mydb');
+
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
 app.use(express.bodyParser());
 
-// For testing purposes:
-contacts = [
-    {
-        name: "Martijn",
-        phone: "1233456",
-        email: "foo@gmail.com"
-    },
-    {
-        name: "Martinus",
-        phone: "44444",
-        email: "foo@bar.com"
-    },
-    {
-        name: "John",
-        phone: "333",
-        email: "foo@bar.com"
-    }
-];
 
 // GET
-// Get contacts, possibly filtered on name, phone or email
+// Gets contacts, possibly filtered by name, phone and/or email
 app.get('/contacts', function(req, res) {
-    partialName = new RegExp(req.param('name'), "gi");
-    partialPhone = new RegExp(req.param('phone'), "gi");
-    partialEmail = new RegExp(req.param('email'), "gi");
-    result = [];
-    for(i=0; i<contacts.length; i++) {
-        if(contacts[i].name.match(partialName) != null && contacts[i].phone.match(partialPhone) != null 
-            && contacts[i].email.match(partialEmail) != null) {
-            result.push(contacts[i]);
+    var db = req.db;
+    var collection = db.get('contacts');
+
+    var partialName = new RegExp(req.param('name'), "gi");
+    var partialPhone = new RegExp(req.param('phone'), "gi");
+    var partialEmail = new RegExp(req.param('email'), "gi");
+
+    collection.find({
+        name: partialName,
+        phone: partialPhone,
+        email: partialEmail
+    }, function(err, doc) {
+        if(err) {
+            res.statusCode = 500;
+            res.send("There was a problem retrieving information from the database.");
+        } else {
+            res.json(doc);
         }
-    }
-    res.json(result);
+    });
+});
+
+// Retrieves one contact by its ID
+app.get('/contacts/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('contacts');
+
+    var id = req.params.id
+
+    collection.findById(id, function(err, doc) {
+        if(err) {
+            res.statusCode = 500;
+            res.send("There was a problem retrieving information from the database.");
+        } else {
+            res.json(doc);
+        }
+    });
 });
 
 
 // POST
+// Adds one contact, name is mandatory
 app.post('/contacts', function(req, res) {
     if(!req.body.hasOwnProperty('name')) {
         res.statusCode = 400;
         return res.send('Error 400: Name attribute missing.');
     } 
+
+    var db = req.db;
+    var collection = db.get('contacts');
  
     var newContact = {
-        name : req.body.name,
+        name : (req.body.name ? req.body.name : ''),
         phone : (req.body.phone ? req.body.phone : ''),
-        email: req.body.email
+        email: (req.body.email ? req.body.email : '')
     };
-   
-    contacts.push(newContact);
-        res.json(true);
+
+    collection.insert(newContact, function (err, doc) {
+        if (err) {
+            // If insert failed, return error
+            res.statusCode = 500;
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            res.json({id: doc._id});
+        }
+    });
 });
 
 
 // DELETE
+// Deletes contacts, possibly filtered by name, phone and/or email
 app.delete('/contacts', function(req, res) {
-    partialName = new RegExp(req.param('name'), "gi");
-    partialPhone = new RegExp(req.param('phone'), "gi");
-    partialEmail = new RegExp(req.param('email'), "gi");
-    for(i=contacts.length-1; i>=0; i--) {
-        if(contacts[i].name.match(partialName) != null && contacts[i].phone.match(partialPhone) != null 
-            && contacts[i].email.match(partialEmail) != null) {
-            contacts.splice(i, 1);
+    var db = req.db;
+    var collection = db.get('contacts');
+
+    var partialName = new RegExp(req.param('name'), "gi");
+    var partialPhone = new RegExp(req.param('phone'), "gi");
+    var partialEmail = new RegExp(req.param('email'), "gi");
+
+    collection.remove({
+        name: partialName,
+        phone: partialPhone,
+        email: partialEmail
+    }, function(err, doc) {
+        if(err) {
+            res.statusCode = 500;
+            res.send("There was a problem removing information from the database.");
+        } else {
+            res.json({nr_contacts_deleted: doc});
         }
-    }
-    res.json(true);
+    });
+});
+
+// Deletes one contact by its ID
+app.delete('/contacts/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('contacts');
+
+    var id = req.params.id
+
+    collection.remove({_id: id}, function(err, doc) {
+        if(err) {
+            res.statusCode = 500;
+            res.send("There was a problem retrieving information from the database.");
+        } else if(doc == 0) {
+            res.statusCode = 404;
+            res.send("Contact with given ID not found.");
+        } else {
+            res.json({nr_contacts_deleted: doc});
+        }
+    });
 });
 
 
