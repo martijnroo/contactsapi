c5326ac3ab6af7aa744ebd2d06637640a00e62d2
Goubin Brendan <goubinb1@niksula.hut.fi>
2014-11-05 17:43:27 +0200

    back to previous changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,51 +1,19 @@
 $(document).ready(function() {
     retrieve('contacts', displayContacts);
-    
-    $('button#backToList').click(function(){
-		retrieve('contacts', displayContacts);
-	});
-	//*/
 });
 
 function displayContacts(contacts) {
-    
-    emptyDisplayContact();
-    
+    var parent = '.all_contacts';
+    $(parent).html('');
     for (var i = 0; i < contacts.length; i++) {
-        $('.all_contacts').append('<button id='+contacts[i]._id+' class="btn btn-primary">'+contacts[i].name+'</button></br>');
+        $(parent).append('<button id='+contacts[i]._id+' class="btn btn-primary">'+contacts[i].name+'</button></br>');
     };
     $('button').click(function() {
         retrieve('contacts/'+this.id, displayContact);
     });
 }
 
-function emptyDisplayContact(){
-	$('.all_contacts').html('');
-}
-
 function displayContact(contact) {
-
-    emptyDisplayContact();
-	var div = generateContactDiv(contact);
-	$('.all_contacts').append(div);
-    
-}
-
-function generateContactDiv(contact){
-	
-	var divElement = $("<div></div>", {class:"contactDisplay", id:contact._id});
-	var table = $("<table></table>");
-	var properties = Object.keys(contact);
-
-	for(var i = 1; i < 4; i++){
-		table.append("<tr><td><b>"+properties[i]+"</b></td><td>"+contact[properties[i]]+"</td></tr>");
-	}
-	divElement.append(table);
-	return divElement;
-	//*/
-}
-function retrieve(request, success_function, data) {
-
     var parent = '.all_contacts';
     $(parent).html('<table><tbody></tbody></table>');
     var properties = Object.keys(contact);
@@ -58,6 +26,23 @@ function retrieve(request, success_function, data) {
     });
     $('.delete').click(function() {
         delete_contact(this.id);
+    });
+}
+
+function retrieve(request, success_function) {
+    $.ajax({
+        url: "http://localhost:8080/"+request,
+        type: 'get',
+        cache: false,
+        async: true,
+        success: function(response) {
+            try {
+                success_function(response);
+            } catch (e) {
+            }
+        },
+        fail: function(response) {
+        }
     });
 }
 
