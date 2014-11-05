fd5fe6d851353089551c20a081687de957ad32e5
Goubin Brendan <goubinb1@niksula.hut.fi>
2014-11-05 20:04:57 +0200

    g

--- public/js/google.js
+++ public/js/google.js
@@ -3,25 +3,26 @@ var apiKey = 'AIzaSyA2CvsroFnZ1p2eLCMwkHlD-9mX9syDFoQ';
 var scopes = 'https://www.google.com/m8/feeds';
 
 $(document).on("click",".js-google_contacts", function(){
+	auth();
 });
-/*
-function appendResults(text) {
-	var results = document.getElementById('results');
-	results.appendChild(document.createElement('P'));
-	results.appendChild(document.createTextNode(text));
-}
 
-function makeRequest() {
-	var request = gapi.client.urlshortener.url.get({'shortUrl': 'http://goo.gl/fbsS'});
-	request.then(function(response) {
-		appendResults(response.result.longUrl);
-	}, function(reason) {
-		console.log('Error: ' + reason.result.error.message);
+
+function auth() {
+	var config = {
+		'client_id': 'OAUTH_CLIENT_ID',
+		'scope': 'https://www.google.com/m8/feeds'
+	};
+	gapi.auth.authorize(config, function() {
+		fetch(gapi.auth.getToken());
 	});
 }
-
-function init() {
-	gapi.client.setApiKey(apiKey);
-	gapi.client.load('urlshortener', 'v1').then(makeRequest);
+ 
+function fetch(token) {
+	$.ajax({
+		url: 'https://www.google.com/m8/feeds/contacts/default/full?alt=json',
+		dataType: 'jsonp',
+		data: token
+	}).done(function(data) {
+		console.log(JSON.stringify(data));
+	});
 }
-*/
