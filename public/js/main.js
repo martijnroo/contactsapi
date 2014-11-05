cfa82e857a9782b9725398ec2d1daede4bffdb4a
Martijn <martijn@720.fi>
2014-11-05 16:03:48 +0200

    Added ability to see a contacts full details

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,42 @@
 $(document).ready(function() {
-
-        $.ajax({
-            url: "http://130.233.42.145:8080/contacts",
-            type: 'get',
-            // data: '',
-            cache: false,
-            async: true,
-            success: function(response) {
-                try {
-                    displayContacts('.all_contacts', response);
-                } catch (e) {
-
-                }
-            },
-            fail: function(response) {
-
-            }
-        });
+    retrieve('contacts', displayContacts);
 });
 
-function displayContacts(parent, contacts) {
-    $(parent).html('<ul></ul>');
+function displayContacts(contacts) {
+    var parent = '.all_contacts';
+    $(parent).html('');
     for (var i = 0; i < contacts.length; i++) {
-        // for(property in contacts[i]) {
-
-        // }
         $(parent).append('<button id='+contacts[i]._id+'>'+contacts[i].name+'</button></br>');
     };
-    $('button').click(function(){
-        console.log(this.id);
-        
+    $('button').click(function() {
+        retrieve('contacts/'+this.id, displayContact);
     });
 }
 
-function displayContact(parent) {
+function displayContact(contact) {
+    var parent = '.all_contacts';
+    $(parent).html('');
+    var properties = Object.keys(contact);
+    for (var i = 0; i < properties.length; i++) {
+        $(parent).append('<tr><td>'+properties[i]+'</td><td>'+contact[properties[i]]+'</td></tr>');
+    };
+    
+}
 
+function retrieve(request, success_function, data) {
+    $.ajax({
+        url: "http://130.233.42.145:8080/"+request,
+        type: 'get',
+        data: '',
+        cache: false,
+        async: true,
+        success: function(response) {
+            try {
+                success_function(response);
+            } catch (e) {
+            }
+        },
+        fail: function(response) {
+        }
+    });
 }
