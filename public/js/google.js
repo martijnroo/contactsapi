92f511fd2a9a6f8c0a4596de5cd82ebf62e82c7b
Goubin Brendan <goubinb1@niksula.hut.fi>
2014-11-05 20:42:56 +0200

    a

--- public/js/google.js
+++ public/js/google.js
@@ -1,28 +1,55 @@
-var clientId = '550013882256-q04gtqu6tkdub380cuaj6r6j0c4mespj.apps.googleusercontent.com';
-var apiKey = 'AIzaSyA2CvsroFnZ1p2eLCMwkHlD-9mX9syDFoQ';
-var scopes = 'https://www.google.com/m8/feeds';
-
-$(document).on("click",".js-google_contacts", function(){
-	auth();
-});
+$(document).ready(function() {
+    gapiLoad();
+}); 
 
 
-function auth() {
-	var config = {
-		'client_id': clientId,
-		'scope': 'https://www.google.com/m8/feeds'
-	};
-	gapi.auth.authorize(config, function() {
-		fetch(gapi.auth.getToken());
+
+function gapiLoad() {
+	gapi.client.setApiKey('AIzaSyDFT_EyN3c_6pG31xUHK52Pm0CFduGDsww'); // app api-wide client api key
+	getGoogleContactEmails(function(result){
+		console.log(result);
 	});
 }
  
-function fetch(token) {
-	$.ajax({
-		url: 'https://www.google.com/m8/feeds/contacts/default/full?alt=json',
-		dataType: 'jsonp',
-		data: token
-	}).done(function(data) {
-		console.log(JSON.stringify(data));
-	});
+function getGoogleContactEmails(callback) {
+	var oauth_clientKey = '550013882256-q04gtqu6tkdub380cuaj6r6j0c4mespj.apps.googleusercontent.com'; // replace with your oauth client api key
+	var firstTry = true;
+	function connect(immediate, callback){
+		var config = {
+			'client_id': oauth_clientKey,
+			'scope': 'https://www.google.com/m8/feeds',
+			'immediate': immediate,
+		};
+		 
+		gapi.auth.authorize(config, function () {
+			var authParams = gapi.auth.getToken();
+			$.ajax({
+				url: 'https://www.google.com/m8/feeds/contacts/default/full?max-results=10000',
+				dataType: 'jsonp',
+				type: "GET",
+				data: authParams,
+				success: function (data) {
+					var parser = new DOMParser();
+					xmlDoc = parser.parseFromString(data,"text/xml");
+					var entries = xmlDoc.getElementsByTagName('feed')[0].getElementsByTagName('entry');
+					var contacts = [];
+					for (var i = 0; i < entries.length; i++){
+						var name = entries[i].getElementsByTagName('title')[0].innerHTML;
+						var emails = entries[i].getElementsByTagName('email');
+						for (var j = 0; j < emails.length; j++){
+							var email = emails[j].attributes.getNamedItem('address').value;
+							contacts.push({name: name, email: email});
+						}
+					}
+					callback(contacts);
+				},
+				error: function (data) {
+					if (firstTry)
+					connect(false, callback);
+					firstTry = false;
+				}
+			})
+		});
+	}
+	connect(true, callback);
 }
