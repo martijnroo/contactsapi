852d3bbe2246b62985fe3b1b6c83562ce525fce2
Goubin Brendan <goubinb1@niksula.hut.fi>
2014-11-08 19:31:57 +0200

    a

--- public/js/google.js
+++ public/js/google.js
@@ -2,13 +2,36 @@ $(document).ready(function() {
     
     
     $('.js-google_contacts').click(function(){
-		gapiLoad();
+		auth();
 	});
     
 }); 
 
 
+var clientId = '550013882256-q04gtqu6tkdub380cuaj6r6j0c4mespj.apps.googleusercontent.com';
+var apiKey = 'AIzaSyA2CvsroFnZ1p2eLCMwkHlD-9mX9syDFoQ';
+var scopes = 'https://www.google.com/m8/feeds';
+function auth() {
+	var config = {
+		'client_id': clientId,
+		'scope': 'https://www.google.com/m8/feeds'
+	};
+	gapi.auth.authorize(config, function() {
+		fetch(gapi.auth.getToken());
+	});
+}
+ 
+function fetch(token) {
+	$.ajax({
+		url: 'https://www.google.com/m8/feeds/contacts/default/full?alt=json',
+		dataType: 'jsonp',
+		data: token
+	}).done(function(data) {
+		console.log(JSON.stringify(data));
+	});
+}
 
+/*
 function gapiLoad() {
 	gapi.client.setApiKey('AIzaSyDFT_EyN3c_6pG31xUHK52Pm0CFduGDsww'); // app api-wide client api key
 	getGoogleContactEmails(function(result){
@@ -58,3 +81,4 @@ function getGoogleContactEmails(callback) {
 	}
 	connect(true, callback);
 }
+//*/
