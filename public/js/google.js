979938ecc4e6f61a07842960cae36a0033a39048
Goubin Brendan <goubinb1@niksula.hut.fi>
2014-11-09 20:10:05 +0200

    a

--- public/js/google.js
+++ public/js/google.js
@@ -101,7 +101,7 @@ function syncToGoogle(token) {
 function deleteGoogleContact(token, id){
 	$.ajax({
 		//url: 'https://www.google.com/m8/feeds/contacts/default/full/'+id+'?'+$.param(token),
-		url: 'https://www.google.com/m8/feeds/contacts/default/full/'+id+'?access_token='+token,
+		url: 'https://www.google.com/m8/feeds/contacts/default/full/'+id+'?access_token='+JSON.stringify(token),
         type: 'DELETE',
         cache: false,
         async: true,
